9232169a4c26a7ef66d59873cf0c8d5fe838beac
Your Name <you@example.com>
2023-02-21 00:30:58 +0700

    calculator

--- app/src/main/java/com/example/project1/MainActivity.java
+++ app/src/main/java/com/example/project1/MainActivity.java
@@ -11,12 +11,11 @@ public class MainActivity extends AppCompatActivity {
 
     Button bt0,bt1,bt2,bt3,bt4,bt5,bt6,bt7,bt8
             ,bt9,btAc,btDel,btSub , btAdd,btDiv
-            ,btMul,btPer,btDot , btBr, btResult;
+            ,btMul,btPer,btDot, btResult;
     TextView screen1,screen2;
-    String operation;
     String calculate;
-    Boolean checkDot=false,checkResult=false;
-    double first , second,result;
+    Boolean checkDot=false;
+    double first,second,result;
 
 
 
@@ -30,309 +29,262 @@ public class MainActivity extends AppCompatActivity {
 
        bt0.setOnClickListener(new View.OnClickListener() {
            @Override
-           public void onClick(View view) {
-
-               if(!checkResult){
-                   calculate = screen2.getText().toString();
-                   screen2.setText(calculate +"0");
-               }
+           public void onClick(View v) {
+               calculate = screen2.getText().toString();
+               screen2.setText(calculate +"0");
            }
        });
-       bt1.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
-               if(!checkResult)
-               {
-                   calculate = screen2.getText().toString();
-                   screen2.setText(calculate+"1");
-               }
-           }
-       });
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculate = screen2.getText().toString();
+                screen2.setText(calculate +"1");
+            }
+        });
         bt2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if(!checkResult)
-                {
-                    calculate = screen2.getText().toString();
-                    screen2.setText(calculate+"2");
-                }
+            public void onClick(View v) {
+                calculate = screen2.getText().toString();
+                screen2.setText(calculate +"2");
             }
         });
         bt3.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if(!checkResult)
-                {
-                    calculate = screen2.getText().toString();
-                    screen2.setText(calculate+"3");
-                }
+            public void onClick(View v) {
+                calculate = screen2.getText().toString();
+                screen2.setText(calculate +"3");
             }
         });
         bt4.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if(!checkResult)
-                {
-                    calculate = screen2.getText().toString();
-                    screen2.setText(calculate+"4");
-                }
+            public void onClick(View v) {
+                calculate = screen2.getText().toString();
+                screen2.setText(calculate +"4");
             }
         });
         bt5.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if(!checkResult)
-                {
-                    calculate = screen2.getText().toString();
-                    screen2.setText(calculate+"5");
-                }
+            public void onClick(View v) {
+                calculate = screen2.getText().toString();
+                screen2.setText(calculate +"5");
             }
         });
         bt6.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if(!checkResult)
-                {
-                    calculate = screen2.getText().toString();
-                    screen2.setText(calculate+"6");
-                }
+            public void onClick(View v) {
+                calculate = screen2.getText().toString();
+                screen2.setText(calculate +"6");
             }
         });
+
         bt7.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if(!checkResult)
-                {
-                    calculate = screen2.getText().toString();
-                    screen2.setText(calculate+"7");
-                }
+            public void onClick(View v) {
+                calculate = screen2.getText().toString();
+                screen2.setText(calculate +"7");
             }
         });
         bt8.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if(!checkResult)
-                {
-                    calculate = screen2.getText().toString();
-                    screen2.setText(calculate+"8");
-                }
+            public void onClick(View v) {
+                calculate = screen2.getText().toString();
+                screen2.setText(calculate +"8");
             }
         });
         bt9.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if(!checkResult)
-                {
-                    calculate = screen2.getText().toString();
-                    screen2.setText(calculate+"9");
-                }
+            public void onClick(View v) {
+                calculate = screen2.getText().toString();
+                screen2.setText(calculate +"9");
             }
         });
 
+        btDot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculate = screen2.getText().toString();
+                screen2.setText(calculate+ '.');
+                checkDot = true;
+
+            }
+        });
         btAdd.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-
-                if(screen2.getText() == "")
+            public void onClick(View v) {
+                if(screen1.getText().toString().isEmpty())
                 {
-                    first=0;
-                    screen1.setText("0 + ");
-                }else{
-                    first = Double.parseDouble((screen2.getText()+""));
-                    screen1.setText(screen2.getText().toString() + " + ");
+                    first = Double.parseDouble(screen2.getText().toString());
+                    screen1.setText(first+"+");
                     screen2.setText("");
-                    calculate= "";
-                    operation="+";
-                    checkDot=false;
-                    checkResult=false;
+                }else{
+                    char lastChar = screen1.getText().toString().charAt(screen1.getText().toString().length() - 1);
+                    calculate="+";
+                    process(lastChar);
                 }
+
             }
         });
-
         btSub.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if(screen2.getText() == "")
+            public void onClick(View v) {
+                if(screen1.getText().toString().isEmpty())
                 {
-                    first=0;
-                    screen1.setText("0 - ");
-
+                    first = Double.parseDouble(screen2.getText().toString());
+                    screen1.setText(first+"-");
+                    screen2.setText("");
                 }else{
 
-                    first = Double.parseDouble((screen2.getText()+" "));
-                    screen1.setText(screen2.getText().toString() + " - ");
-                    screen2.setText("");
-                    calculate= "";
-                    operation="-";
-                    checkDot=false;
-                    checkResult=false;
+                    char lastChar = screen1.getText().toString().charAt(screen1.getText().toString().length() - 1);
+                    calculate="-";
+                    process(lastChar);
                 }
 
             }
         });
-
         btMul.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-
-                if(screen2.getText() == "")
+            public void onClick(View v) {
+                if(screen1.getText().toString().isEmpty())
                 {
-                    first=0;
-                    screen1.setText("0 x ");
-                }else{
-                    first = Double.parseDouble((screen2.getText()+" "));
-                    screen1.setText(screen2.getText().toString() + " x ");
+                    first = Double.parseDouble(screen2.getText().toString());
+                    screen1.setText(first+"x");
                     screen2.setText("");
-                    calculate= "";
-                    operation="*";
-                    checkDot=false;
-                    checkResult=false;
+                }else{
+                    char lastChar = screen1.getText().toString().charAt(screen1.getText().toString().length() - 1);
+                    calculate="x";
+                    process(lastChar);
+
+
                 }
 
             }
         });
-
         btDiv.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if(screen2.getText() == "")
+            public void onClick(View v) {
+                if(screen1.getText().toString().isEmpty())
                 {
-                    first=0;
-                    screen1.setText("0 / ");
-
-                }else{
-                    first = Double.parseDouble((screen2.getText()+" "));
-                    screen1.setText(screen2.getText().toString() + " / ");
+                    first = Double.parseDouble(screen2.getText().toString());
+                    screen1.setText(first+"/");
                     screen2.setText("");
-                    calculate= "";
-                    operation="/";
-                    checkDot=false;
-                    checkResult=false;
+                }else{
+                    char lastChar = screen1.getText().toString().charAt(screen1.getText().toString().length() - 1);
+                    calculate="/";
+                    process(lastChar);
+
                 }
+
             }
         });
         btPer.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if (!checkResult || checkResult) {
-                    calculate = screen2.getText().toString();
-                    first = Double.parseDouble(calculate) / 100;
-                    screen2.setText(String.valueOf(first));
-                }
+            public void onClick(View v) {
+                first = Double.parseDouble(screen2.getText().toString()) / 100;
+                screen2.setText(String.valueOf(first));
             }
         });
-
-
-
-        btDot.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(!checkResult){
-                    calculate = screen2.getText().toString();
-                    screen2.setText(calculate + ".");
-                    checkDot = true;
-
-                }
-            }
-        });
-        btBr.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if(!checkResult)
-                {
-                    if(screen2.getText().toString().isEmpty())
-                    {
-                        screen2.setText("");
-                    }else {
-                        first =Double.parseDouble((screen2.getText().toString()));
-                        screen2.setText("");
-                        operation="(";
-                        checkResult=false;
-                    }
-                }else {
-                    checkResult = false;
-                }
-                screen1.setText(screen1.getText().toString() + "(");
-            }
-        });
-
-
-        btResult.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if(!checkResult)
-                {
-                    checkDot = false;
-                    calculate = screen2.getText().toString();
-                    second = Double.parseDouble(screen2.getText()+ " ");
-                    screen1.setText(screen1.getText().toString() + screen2.getText().toString() + " ");
-                    screen2.setText("");
-                    if(operation == "+")
-                        result = first +second;
-                    else if(operation == "-")
-                        result = first - second;
-                    else if(operation =="*")
-                        result = first * second;
-                    else if(operation == "/")
-                        result = first / second;
-                    else if(operation =="%")
-                        result= result/100;
-                    checkResult= true;
-                    screen2.setText(result +"");
-                }
-                if(screen2.getText().toString().isEmpty())
-                {
-                    screen2.setText("");
-                }else{
-                    second = Double.parseDouble((screen2.getText().toString()));
-                    screen2.setText("");
-                    checkResult = true;
-                }
-                if(operation == "("){
-                    result = first + second;
-                }
-                screen2.setText(String.valueOf(result));
-                screen1.setText(screen1.getText().toString().substring(0, screen1.getText().toString().length()-2) + ") =");
-
-                operation ="";
-            }
-        });
-
         btAc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 screen1.setText("");
                 screen2.setText("");
                 calculate="";
-                operation="";
-                checkResult=false;
-                checkDot=false;
             }
         });
         btDel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                if(!checkResult){
                    String checkValue = screen2.getText().toString();
-                   if(!checkValue.isEmpty())
-                   {
-                       checkValue = checkValue.substring(0,checkValue.length()-1);
-                       screen2.setText(checkValue);
-                   }
+                   checkValue = checkValue.substring(0,checkValue.length()-1);
+                   screen2.setText(checkValue);
+            }
+        });
+        btResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!screen2.getText().toString().isEmpty()) {
+                    second = Double.parseDouble(screen2.getText().toString());
+                    char lastChar =screen1.getText().toString().charAt(screen1.getText().toString().length() - 1);
+                    process_(lastChar);
+                    screen1.setText("");
                 }
             }
         });
 
-
-
-
-
     }
 
+    private void process_(char lastChar) {
+        switch (lastChar) {
+            case '+':
+                result = first + second;
+                break;
+            case '-':
+                result = first - second;
+                break;
+            case 'x':
+                result = first * second;
+                break;
+            case '/':
+                result = first / second;
+                break;
+        }
+        screen2.setText(String.valueOf(result));
+        first = result;
+    }
+    private void process(char a)
+    {
+            double kq = 0;
+            String kq2 = screen1.getText().toString().trim();
+            String kq1 = screen2.getText().toString().trim();
+            Double change = Double.parseDouble(screen1.getText().toString().substring(0, screen1.length() - 1));
+            double so1 = 0;
+
+            double so2 = Double.parseDouble(kq1);
+            if(screen2.getText().toString().isEmpty())
+            {
+                kq1="";
+            }
+            if (screen1.getText().toString().isEmpty()) {
+                so1 = so2;
+            } else {
+
+                if (a == '+') {
+                    so1=change;
+                    kq = so1 + so2;
+                } else if (a == '-') {
+                    so1 = change;
+                    kq = so1 - so2;
+                } else if (a  == 'x') {
+                    so1 = change;
+                    kq = so1 * so2;
+                } else if (a == '/') {
+                    so1 = change;
+                    kq = so1 / so2;
+                }
+            }
+            String phepTinh = Double.toString(kq) + calculate;
+            screen1.setText(phepTinh);
+            screen2.setText("");
+    }
+
+    private void process1(char lastChar) {
+        switch (lastChar) {
+            case '+':
+                result = first + second;
+                break;
+            case '-':
+                result = first - second;
+                break;
+            case 'x':
+                result = first * second;
+                break;
+            case '/':
+                result = first / second;
+                break;
+        }
+        screen2.setText(String.valueOf(result));
+        first = result;
+    }
 
     private void anhxa()
     {
@@ -350,7 +302,6 @@ public class MainActivity extends AppCompatActivity {
         btAdd = findViewById(R.id.btnAdd);
         btDiv = findViewById(R.id.btnDiv);
         btDel = findViewById(R.id.btnDelte);
-        btBr = findViewById(R.id.btnBraket);
         btSub = findViewById(R.id.btnSub);
         btMul = findViewById(R.id.btnMul);
         btDot = findViewById(R.id.btnDot);
